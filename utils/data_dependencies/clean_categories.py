e3fb5c52595ade53cce43a2059f4bf0e39f6f8fd
thegraphn <aurelien.levecq@gmail.com>
2020-03-01 18:05:27 +0100

    merging articles 1st version

--- utils/data_dependencies/clean_categories.py
+++ utils/data_dependencies/clean_categories.py
@@ -1,37 +1,36 @@
-from utils import getColumnIndex,getContentColumn,writeFile
-import sys
-from collections import Counter
+import os,sys
+import tqdm
+from data_processing.utils.utils import createMappingBetween2Columns, getMappingColumnIndex
 from nltk import word_tokenize
 import csv
-csv_file = "datafeed.csv"
-index_Sex = getColumnIndex(csv_file,"Fashion:suitable_for",",")
-index_titel = getColumnIndex(csv_file,"Title",",")
-index_description = getColumnIndex (csv_file,"description",",")
-index_category = getColumnIndex(csv_file,"category_name",",")
+csv_file = r"C:\Users\graphn\PycharmProjects\you_conscious\data_processing\data_working_directory\filtered" \
+           r"\labeled_data_feed.csv "
 
-def createMapping(csv_file):
-    mapping = {}
-    with open(csv_file,encoding="utf-8") as f:
-        csv_reader = csv.reader(f,delimiter =";")
-        for row in csv_reader:
-            mapping[row[2]] = row[6]
-    return mapping
+index_Sex = getMappingColumnIndex(csv_file,",")["Fashion:suitable_for"]
+index_title = getMappingColumnIndex(csv_file, ",")["Title"]
+index_description = getMappingColumnIndex (csv_file,",")["description"]
+index_category = getMappingColumnIndex(csv_file,",")["category_name"]
 
-mapping = createMapping("/Users/ConnyContini/Downloads/backend_backup_2019-4-29/utils/category/categoriesCleaning_mapping.csv")
-output_csv_file = "/Users/ConnyContini/Downloads/DF_recategorized.csv"
+
+mapping = createMappingBetween2Columns(file=r"C:\Users\graphn\PycharmProjects\you_conscious\utils\data_dependencies"
+                                            r"\categoriesCleaning_fashionSuitableFor_mapping.csv",
+                                       column1_id=2, column2_id=6, delimiter=";")
+output_csv_file = r"C:\Users\graphn\PycharmProjects\you_conscious\data_processing\data_working_directory\filtered" \
+                  r"\cleansed_data_feed.csv "
 with open(csv_file,encoding="utf-8") as f:
     with open(output_csv_file,'w',encoding="utf-8") as o:
         csv_writer = csv.writer(o)
         csv_reader = csv.reader(f)
-        for row in csv_reader:
+        num_lines = sum(1 for line in open(csv_file,encoding="utf-8"))
+        for row in tqdm.tqdm(csv_reader,total=num_lines):
             sexe = row[index_Sex]
-            words_title = word_tokenize(row[index_titel])
+            words_title = word_tokenize(row[index_title])
             for word in words_title:
                 for k,v in mapping.items():
                     if word == k:
                         row[index_category] = v
                         cat = row[index_category].split(" > ")
-                        if len(cat)!=0:
+                        if len(cat) >= index_Sex:
                             cat[1] = row[index_Sex]
                             if cat[1] == "" or cat[1] == " ":
                                 cat[1] = "Damen"
@@ -41,8 +40,10 @@ with open(csv_file,encoding="utf-8") as f:
                             cat = cat.replace("male","Herren")
                             cat = cat.replace("Male", "Herren")
                             break
+
                     else:
                         cat = row[index_category]
 
-            row[index_category] = cat
+
+            row[index_category] = str(cat)
             csv_writer.writerow(row)
